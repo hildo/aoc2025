fa18312d570640d1d2de40e4283d42d9c1bb1fd5
Ed Hillmann <edhillmann@yahoo.com>
2025-12-01 22:50:35 +1000

    Part two

--- src/day01.rs
+++ src/day01.rs
@@ -35,6 +35,36 @@ fn turn_dial(current_position: &i32, combo: &str) -> i32 {
     new_position
 }
 
+fn turn_dial_2(current_position: &i32, combo: &str) -> (i32, i32) {
+    let mut new_position = *current_position;
+    let direction = combo.chars().nth(0).unwrap();
+    let mut clicks: i32 = combo[1..].parse().unwrap();
+    let mut crossing_count = clicks / 100;
+    clicks = clicks % 100;
+    match direction {
+        'L' => {
+            new_position -= clicks;
+            if new_position < 0 {
+                new_position = 100 - new_position.abs();
+                if new_position != 0 && *current_position != 0 {
+                    crossing_count += 1;
+                }
+            }
+        }
+        'R' => {
+            new_position += clicks;
+            if new_position > 99 {
+                new_position = new_position - 100;
+                if new_position != 0 && *current_position != 0 {
+                    crossing_count += 1;
+                }
+            }
+        }
+        _ => {}
+    }
+    (new_position, crossing_count)
+}
+
 fn find_zeros(starting_point: i32, combinations: &Vec<String>) -> i32 {
     let mut count = 0;
     let mut position = starting_point;
@@ -47,6 +77,19 @@ fn find_zeros(starting_point: i32, combinations: &Vec<String>) -> i32 {
     count
 }
 
+fn find_zeros_2(starting_point: i32, combinations: &Vec<String>) -> i32 {
+    let mut count = 0;
+    let mut position = starting_point;
+    for combo in combinations {
+        let result = turn_dial_2(&position, combo);
+        if result.0 == 0 {
+            count += 1;
+        }
+        count += result.1;
+        position = result.0;
+    }
+    count
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,6 +116,21 @@ mod tests {
     }
 
     #[test]
+    fn test_day01_2_simple() {
+        // Example test case for day01 module
+        let combinations = load_combinations_from_file("./src/resources/day01_simple.txt");
+        let zeros = find_zeros_2(50, &combinations);
+        assert_eq!( zeros, 6);
+    }
+
+
+    #[test]
+    fn test_turn_dial_2() {
+        let res = turn_dial_2(&0, "R150");
+        assert_eq!(res, (50, 1));
+    }
+
+    #[test]
     fn test_day01() {
         // Example test case for day01 module
         let combinations = load_combinations_from_file("./src/resources/day01_input.txt");
@@ -80,6 +138,17 @@ mod tests {
         println!("Number of zeros: {}", zeros);
         // assert_eq!( zeros, 3);
     }    
+
+    #[test]
+    fn test_day01_part2() {
+        // Example test case for day01 module
+        let combinations = load_combinations_from_file("./src/resources/day01_input.txt");
+        let zeros = find_zeros_2(50, &combinations);
+        println!("Number of zeros: {}", zeros);
+        // assert_eq!( zeros, 3);
+    }    
+
+    
     #[test]
     fn test_modulo() {
         let value = -43 % 100;
